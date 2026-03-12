a69aec11d6b85954ae93e080ed2b6e6b51f751e1
Adam Duda <adam.duda@post.com>
2026-03-12 21:30:16 +0100

    First fix ('Contact button')

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Szkopuł Utils is a browser extension that provides various QOL improvements and
 
 ## Full list of features:
 
-none yet lol
+- Hides the 'Contact' (or 'Send feedback' in english) button in the bottom left corner of the footer. It previously caused bottom part of the website's elements to be non-interactable (because of bad flex-box styling and `z-index: 999`).
 
 ## Contributing
 
--- src/worker.ts
+++ src/worker.ts
@@ -4,3 +4,13 @@ console.log('worker');
 console.log('worker');
 console.log('worker');
 console.log('worker');
+
+fix_contact_button();
+
+
+function fix_contact_button() {
+    document.getElementById('szkopul-contact-form-open-div')!.style.position = 'absolute';
+    document.getElementById('szkopul-contact-form-open-div')!.style.left = '5px';
+    document.getElementById('szkopul-contact-form-open-div')!.style.bottom = '5px';
+    document.getElementById('szkopul-contact-form-open-div')!.style.width = 'auto';
+}
