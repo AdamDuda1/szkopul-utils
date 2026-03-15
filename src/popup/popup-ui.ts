6cad20108e80b06ec38ec346471695d159ce2ec5
Adam Duda <adam.duda@post.com>
2026-03-15 11:27:00 +0100

    Moved the rest of the popup tabs to popup-ui

--- src/popup/popup-ui.ts
+++ src/popup/popup-ui.ts
@@ -33,3 +33,59 @@ render(
         </div>
 	`, document.getElementById('home')!
 );
+
+render(
+	html`
+        <div style="position: absolute; top: 5px; left: 5px; display: flex; height: 50px">
+            <button class="btn btn-default" id="btn-backHome-TODO"
+                    style="width: auto; padding: 3px; color: white;     display: flex;">
+                <img src="../icons/ic-arrow-right.svg" alt="" class="back-btn">
+                <h3 style="position: relative; top: -4px; left: 5px;">Do zrobienia</h3>
+            </button>
+        </div>
+        
+        <br><br>
+        
+
+        <button type="button" class="btn btn-info">Otwórz pełną listę</button>
+	`, document.getElementById('todo')!
+);
+
+render(
+	html`
+        <div>
+            <div style="position: absolute; top: 5px; left: 5px; display: flex; height: 50px">
+                <button class="btn btn-default" id="btn-backHome-options"
+                        style="width: auto; padding: 3px; color: white; display: flex;">
+                    <img src="../icons/ic-arrow-right.svg" alt="" class="back-btn">
+                    <h3 style="position: relative; top: -4px; left: 5px;">Opcje</h3>
+                </button>
+            </div>
+        </div>
+
+        <br><br>
+
+        <div class="form-check form-switch switch-full d-flex align-items-center justify-content-between">
+            <label for="hideScoresOption" class="mb-0 form-check-label">Ukrywaj wyniki</label>
+            <div class="form-check form-switch m-0">
+                <input class="form-check-input" type="checkbox" id="hideScoresOption">
+            </div>
+        </div>
+
+        <div class="form-check form-switch switch-full d-flex align-items-center justify-content-between">
+            <label for="hideTimerOption" class="mb-0 form-check-label">Ukrywaj czasomierze</label>
+            <div class="form-check form-switch m-0">
+                <input class="form-check-input" type="checkbox" id="hideTimerOption">
+            </div>
+        </div>
+
+
+        <button type="button" class="btn btn-warning b">Export danych</button>
+        <button type="button" class="btn btn-danger b">Usuń wszystkie dane</button>
+
+        <!--        <div class="form-group">-->
+        <!--            <label for="exampleInputFile">Import danych</label>-->
+        <!--            <input type="file" id="exampleInputFile" placeholder="Wybierz plik z danymi">-->
+        <!--        </div>-->
+	`, document.getElementById('options')!
+)
